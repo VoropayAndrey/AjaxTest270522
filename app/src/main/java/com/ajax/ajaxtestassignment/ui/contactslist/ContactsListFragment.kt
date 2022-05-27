e6ca367de322ef946010173e9f2059360e15b6a0
Voropay Andrey <andrody.man@gmail.com>
2022-05-27 14:21:15 +0300

    Added regeneration button

--- app/src/main/java/com/ajax/ajaxtestassignment/ui/contactslist/ContactsListFragment.kt
+++ app/src/main/java/com/ajax/ajaxtestassignment/ui/contactslist/ContactsListFragment.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.ajax.ajaxtestassignment.R
 import com.ajax.ajaxtestassignment.databinding.FragmentContactsListBinding
 import com.ajax.ajaxtestassignment.di.GlobalFactory
 import com.ajax.ajaxtestassignment.domain.entities.ContactEntity
@@ -47,6 +49,11 @@ open class ContactsListFragment : Fragment() {
                 binding = it
             }
             .root
+
+        view.findViewById<Button>(R.id.generateAgain).setOnClickListener {
+            GlobalFactory.settingsRepo.setFirstRun(true)
+            contactsListViewModel.fetchUsers()
+        }
 
         contactsListViewModel.contactsLiveData.observe(viewLifecycleOwner, Observer {
             contactAdapter.items = it
